a31bf4e39d49a767a9fd66be9466fbf85a1adb27
DyRoss5 <dyamondross5@gmail.com>
2024-07-03 11:34:13 -0500

    <Strings>

--- loops/exercises/for-Loop-Exercises.js
+++ loops/exercises/for-Loop-Exercises.js
@@ -3,22 +3,53 @@
     b. Print only the ODD values from 3 - 29, one number per line.
     c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
     d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers). */
+for (let a = 0; a <= 20; a++){
+  console.log(a);
+}
 
+for (let b = 3; b <= 29; b+=2){
+  console.log(b);
+}
 
+for (let c = 12; c >= -14; c-=2){
+  console.log(c);
+}
 
 
 /*Exercise #2: 
 Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].
 
-
 Construct ``for`` loops to accomplish the following tasks:
   a. Print each element of the array to a new line.
   b. Print each character of the string - in reverse order - to a new line. */ 
 
-
-
+let str = "LaunchCode";
+let newArray = [1, 5, 'LC101', 'blue', 42];
+let reversed = "";
+for (let i = 0; i < newArray.length; i++) {
+  console.log(newArray[i]);
+}
+ for (let i =0; i < str.length; i++) {
+  reversed = str[i] + reversed;
+ }
+ console.log(reversed);
 
 
 /*Exercise #3:Construct a for loop that sorts the array [2, 3, 13, 18, -5, 38, -10, 11, 0, 104] into two new arrays:
   a. One array contains the even numbers, and the other holds the odds.
   b. Print the arrays to confirm the results. */
+
+  let arr = [2, 3, 13, 18, -5, 38, -10, 11, 0, 104];
+  let evens = [], odds = []; 
+
+  for (let i=0; i < arr.length; i++) {
+    if (arr[i] % 2) {
+      evens.push(arr[i]);
+      console.log(evens);
+    } else {
+      odds.push(arr[i]);
+      console.log(odds);
+    }
+  };
+console.log(evens);
+console.log(odds);
--- loops/studio/solution.js
+++ loops/studio/solution.js
@@ -2,9 +2,9 @@ const input = require('readline-sync');
 
 // Part A: #1 Populate these arrays
 
-let protein = [];
-let grains = [];
-let veggies = [];
+let protein = ['chicken', 'pork', 'tofu', 'beef', 'fish', 'beans'];
+let grains = ['rice', 'pasta', 'corn', 'potato', 'quinoa', 'crackers'];
+let veggies =['rice', 'pasta', 'corn', 'potato', 'quinoa', 'crackers'] ;
 let beverages = [];
 let desserts = [];
 
@@ -15,17 +15,33 @@ function mealAssembly(protein, grains, veggies, beverages, desserts, numMeals) {
   
   /// Part A #2: Write a ``for`` loop inside this function
   /// Code your solution for part A #2 below this comment (and above the return statement) ... ///
-
+    /// Code your solution for part A #2 here ... it will be a nested loop ... ///
+    function mealAssembly(protein, grains, veggies, beverages, desserts, n) {
+      let pantry = [protein, grains, veggies, beverages, desserts];
+      let meals = [];
+      /// Code your solution for part A #2 here ... it will be a nested loop ... ///
+      for (let i = 0; i < n; i++) {
+            let meal = [];
+            for (let j = 0; j < pantry.length; j++) {
+                meal.push(pantry[j][i]);
+                // console.log()
+            }
+            meals.push(meal);
+        }
+      return meals;
+    }
 
   return meals;
-}
+  }
 
 
 function askForNumber() {
-  numMeals = input.question("How many meals would you like to make?");
+ let numMeals = input.question("How many meals would you like to make?");
   
   /// CODE YOUR SOLUTION TO PART B here ///
-
+while (numMeals < 1 || numMeals > 6) {
+  numMeals = input.question("How many meals would you like to make?");
+}
   return numMeals;
 }
 
@@ -33,6 +49,10 @@ function askForNumber() {
 function generatePassword(string1, string2) {
   let code = '';
 
+  for (i = 0; i < string1.length || i < string2.length; i++) {
+    code += string1[i];
+    code += string2[i];
+  }
   /// Code your Bonus Mission Solution here ///
 
   return code;
@@ -45,24 +65,24 @@ function runProgram() {
   /// Change the final input variable (aka numMeals) here to ensure your solution makes the right number of meals ///
   /// We've started with the number 2 for now. Does your solution still work if you change this value? ///
   
-  // let meals = mealAssembly(protein, grains, veggies, beverages, desserts, 2);
-  // console.log(meals)
+   let mealsArr = mealAssembly(protein, grains, veggies, beverages, desserts, 2);
+  console.log(mealsArr)
   
 
   /// TEST PART B HERE ///
   /// UNCOMMENT the next two lines to test your ``askForNumber`` solution ///
   /// Tip - don't test this part until you're happy with your solution to part A #2 ///
   
-  // let mealsForX = mealAssembly(protein, grains, veggies, beverages, desserts, askForNumber());
-  // console.log(mealsForX);
+  let mealsForX = mealAssembly(protein, grains, veggies, beverages, desserts, askForNumber());
+  console.log(mealsForX);
 
     /// TEST PART C HERE ///
   /// UNCOMMENT the remaining commented lines and change the password1 and password2 strings to ensure your code is doing its job ///
 
-  // let password1 = '';
-  // let password2 = '';
-  // console.log("Time to run the password generator so we can update the menu tomorrow.")
-  // console.log(`The new password is: ${generatePassword(password1, password2)}`);
+  let password1 = '1234';
+  let password2 = '5678';
+  console.log("Time to run the password generator so we can update the menu tomorrow.")
+  console.log(`The new password is: ${generatePassword(password1, password2)}`);
 }
 
 module.exports = {
